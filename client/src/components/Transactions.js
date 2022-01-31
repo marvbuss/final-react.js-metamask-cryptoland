892451c63d1810ddf5794202fc723f325c647696
marvinbuss <marvin.buss24@gmail.com>
2022-01-31 21:01:34 +0100

    add transaction card component

--- client/src/components/Transactions.js
+++ client/src/components/Transactions.js
@@ -1,5 +1,67 @@
+import React, { useContext } from "react";
+import {
+    TransactionContext,
+    TransactionsContext,
+} from "../context/TransactionContext";
+
+import { dummyData } from "../utils/dummyData";
+
+const TransactionCard = ({
+    address,
+    timestamp,
+    message,
+    keyword,
+    amount,
+    addressTo,
+    url,
+}) => {
+    return (
+        <div className="transaction-card">
+            <div className="transaction">
+                <a
+                    href={`https://ropsten.etherscan.io/address/${address}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <p>From: {address}</p>
+                </a>
+                <a
+                    href={`https://ropsten.etherscan.io/address/${address}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <p>To: {addressTo}</p>
+                </a>
+                <p>Amount: {amount}</p>
+                <p>Message: {message}</p>
+                <p>{timestamp}</p>
+            </div>
+        </div>
+    );
+};
+
 function Transactions() {
-    return <h1>Transactions</h1>;
+    const { walletAddress } = useContext(TransactionsContext);
+    return (
+        <>
+            <div className="transactions-container">
+                <div className="transactions-wrapper">
+                    {walletAddress ? (
+                        <h1>Latest Transactions</h1>
+                    ) : (
+                        <h1>
+                            Connect your account to see the Latest Transactions
+                        </h1>
+                    )}
+                    <div className="transactions">
+                        {dummyData.reverse().map((transaction, i) => {
+                            return <TransactionCard key={i} {...transaction} />;
+                        })}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Transactions;
